8983fcdf50314cd4b1754836b1fd8202119a86b7
gavin <gavin.gao@e3-expo.com>
2018-07-06 15:18:31 +0800

    init

--- src/js/main_xc.js
+++ src/js/main_xc.js
@@ -1,15 +1,14 @@
 $(function () {
   /**
-   * 功能介绍,每个方法请写注释，按下面模板来写
-   * @author <作者> 
+   * 功能介绍,每个方法请写注释，按下面模板来写.
+   * @author <作者>
    * @param {类型} 参数名 描述.
    * @param {string} name=alice 姓名(默认alice).
    * @param {object} option 配置信息.
-   * @return {Number} 描述
+   * @return {Number} 返回值描述.
    */
   function name (name, option) {
     return 1
   }
-
+  name(1, 2)
 })
-
