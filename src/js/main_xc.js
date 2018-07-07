96f5f68c6f12904a66f39c3c946a20e7a71e9030
xiaocheng <707996290@qq.com>
2018-07-07 15:34:26 +0800

    '..'

--- src/js/main_xc.js
+++ src/js/main_xc.js
@@ -7,8 +7,74 @@ $(function () {
    * @param {object} option 配置信息.
    * @return {Number} 返回值描述.
    */
-  function name (name, option) {
-    return 1
+  var commont = {
+    /**
+     * 初始化
+     */
+    init: function () {
+      this.initFontSize()
+      this.addLoginEvent()
+      this.addSliderBarEvent()
+    },
+    /**
+     * 添加侧边栏事件
+     */
+    addSliderBarEvent () {
+      $('.comm-header .comm-operating').click(function () {
+        $('body').addClass('slider-bar-open')
+      })
+      $('.wrap .markBg').click(function () {
+        $('body').removeClass('slider-bar-open')
+      })
+    },
+    /**
+     * 添加登录页面的一些事件
+     */
+    addLoginEvent () {
+      var $reviewDom = $('#review'),
+        $user = $('#user'),
+        $close = $reviewDom.siblings('.login-input-close')
+      $reviewDom.keyup(function () {
+        var $review = $(this).val().trim()
+        if ($review) {
+          $close.show(200)
+        } else {
+          $close.hide(200)
+        }
+      })
+      $close.click(function () {
+        $reviewDom.val('')
+        $(this).hide(200)
+      })
+      $user.keyup(function () {
+        var $this = $(this),
+          user = $this.val().trim()
+        if (user) {
+          $this.addClass('active')
+        } else {
+          $this.removeClass('active')
+        }
+      })
+    },
+    /**
+     * 初始化根目录节点
+     */
+    initFontSize: function () {
+      function change () {
+        var cWidth
+        if (document.documentElement.clientWidth < 320) {
+          cWidth = 320
+        } else if (document.documentElement.clientWidth > 750) {
+          cWidth = 750
+        } else {
+          cWidth = document.documentElement.clientWidth
+        };
+        document.documentElement.style.fontSize = cWidth / 18.75 + 'px'
+      }
+      change()
+      window.addEventListener('resize', change, false)
+      window.addEventListener('DOMContentLoaded', change, false)
+    }
   }
-  name(1, 2)
+  commont.init()
 })
