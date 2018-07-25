210efc1aaa1eebd4de70ae6cb14575b6fd19b4c4
gavin <gavin.gao@e3-expo.com>
2018-07-25 17:33:18 +0800

    gulp 优化

--- build/gulpfile.js
+++ build/gulpfile.js
@@ -151,11 +151,7 @@ gulp.task('server', ['dev'], () => {
   })
   gulp.watch(config.css.src, (event)=> {
     console.log('[项目信息] 文件 [' + event.path + '] 有变动,状态为:' + event.type)
-    sequence(['stylelint'])(() => {
-      setTimeout(function(){
-        sequence(['postcss'])()
-      }, 500)
-    })
+    sequence(['stylelint'], ['postcss'])()
   })
   gulp.watch(config.js.src, ['standard'])
   gulp.watch(config.html.watch, ['html'])
