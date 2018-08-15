6878b2c7283aacd19c5e44eb2b484b48baf5e0de
xiaocheng <707996290@qq.com>
2018-08-15 21:32:17 +0800

    es6 bug
    es6 bug

--- src/js/main_wj.js
+++ src/js/main_wj.js
@@ -1,25 +1,20 @@
+'use strict'
+
 $(function () {
   /**
    * 弹框
    * title 弹框标题
    * callback 回调函数
    */
-  function bomb ({title = '确认清空所有未读消息？'}, callback) {
-    let $str = $(`<div class="bomb-wp">
-                <div class="bomb-ct">
-                  <img src="../../img/bomb_icon.png" alt="" class="bomb-icon">
-                  <p class="bomb-title">${title}</p>
-                  <div class="btn-wp clearfix">
-                    <i class="line"></i>
-                    <a data-close="0" href="javascript:;" class="cancle-btn">取消</a>
-                    <a data-close="1" href="javascript:;" class="sure-btn">确认</a>
-                  </div>
-                </div>
-              </div>`)
+  function bomb (_ref, callback) {
+    var _ref$title = _ref.title,
+      title = _ref$title === undefined ? '确认清空所有未读消息？' : _ref$title
+
+    var $str = $('<div class="bomb-wp">\n                <div class="bomb-ct">\n                  <img src="../../img/bomb_icon.png" alt="" class="bomb-icon">\n                  <p class="bomb-title">' + title + '</p>\n                  <div class="btn-wp clearfix">\n                    <i class="line"></i>\n                    <a data-close="0" href="javascript:;" class="cancle-btn">\u53D6\u6D88</a>\n                    <a data-close="1" href="javascript:;" class="sure-btn">\u786E\u8BA4</a>\n                  </div>\n                </div>\n              </div>')
     $str.appendTo('body').show()
     $str.find('[data-close]').click(function (e) {
       $str.remove()
-      let dataclose = $(e.target).attr('data-close')
+      var dataclose = $(e.target).attr('data-close')
       callback(dataclose)
     })
   }
@@ -28,7 +23,12 @@ $(function () {
    * el 滑动元素
    */
   function leftslideDelete (el) {
-    let [touchstartX, touchstartY, touchmoveX, touchmoveY, touchendX] = [0, 0, 0, 0, 0]
+    var touchstartX = 0,
+      touchstartY = 0,
+      touchmoveX = 0,
+      touchmoveY = 0,
+      touchendX = 0
+
     $(el).on('touchstart', function (event) {
       touchstartX = event.changedTouches[0].pageX
       touchstartY = event.changedTouches[0].pageY
@@ -37,24 +37,26 @@ $(function () {
     $(el).on('touchmove', function (event) {
       touchmoveX = event.changedTouches[0].pageX
       touchmoveY = event.changedTouches[0].pageY
-      let distanceX = Math.abs(touchmoveX - touchstartX)
-      let distanceY = Math.abs(touchmoveY - touchstartY)
+      var distanceX = Math.abs(touchmoveX - touchstartX)
+      var distanceY = Math.abs(touchmoveY - touchstartY)
       // console.log(distanceX, distanceY)
       if (distanceX > distanceY) {
         event.preventDefault()
       }
     })
-    let canslide // 1代表可滑动 2代表上一个滑动的
+    var canslide = void 0 // 1代表可滑动 2代表上一个滑动的
     $(el).on('touchend', function (event) {
       touchendX = event.changedTouches[0].pageX
-      let distanceX = touchendX - touchstartX
+      var distanceX = touchendX - touchstartX
       $('.delete-btn').html('删除')
-      if (distanceX > 0) { // 左滑
+      if (distanceX > 0) {
+        // 左滑
         $(el).animate({
           marginLeft: '0'
         }, 500)
         $(this).data('canslide', 1)
-      } else if (distanceX < 0 && Math.abs(distanceX) > 50) { // 右滑
+      } else if (distanceX < 0 && Math.abs(distanceX) > 50) {
+        // 右滑
         canslide = $(this).data('canslide')
         if (canslide === 1) {
           $(el).each(function () {
@@ -82,14 +84,14 @@ $(function () {
    */
   $('.delete-btn').on('click', function () {
     if ($(this).html() === '确认删除') {
-      let len = $(this).parents('.msg-ul').find('li').length
+      var len = $(this).parents('.msg-ul').find('li').length
       len = len - 1
       if (!len) {
         $(this).parents('ul').prev('.date-txt').remove()
       }
       $(this).parents('li').remove()
     }
-    let _this = this
+    var _this = this
     $(_this).html('确认删除')
   })
   /**
